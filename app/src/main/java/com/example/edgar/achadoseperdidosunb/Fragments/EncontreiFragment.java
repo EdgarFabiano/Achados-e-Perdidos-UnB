6ac66d0bd1f5a358c2398557d2c8dc5512390440
Rennê Ruan <140030930@aluno.unb.br>
2016-11-06 20:01:45 -0200

    Quebrando o projeto com telas feias <3

--- app/src/main/java/com/example/edgar/achadoseperdidosunb/Fragments/EncontreiFragment.java
+++ app/src/main/java/com/example/edgar/achadoseperdidosunb/Fragments/EncontreiFragment.java
@@ -18,7 +18,7 @@ public class EncontreiFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         //Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_encontrei, container, false);
+        return inflater.inflate(R.layout.fragment_registrar_objeto, container, false);
 
     }
 
